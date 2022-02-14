93c26e614e36dba7adfc058b3bebc1e8876be5a7
KharinMikhail <misha.xaren2011@mail.ru>
2022-02-14 10:50:36 +0700

    Add project files.

---
+++ ApiTestingProject/Program.cs
@@ -0,0 +1,19 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddMvc();
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+fsadad
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapControllers();
+
+app.Run();
